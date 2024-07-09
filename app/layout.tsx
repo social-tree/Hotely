24b32e582ad0393a6836996722c9f7ea3f827afa
selly361 <seikou615@gmail.com>
2024-07-09 19:41:42 +0100

    Update: add template title on metadata, and change description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,34 @@
 import { Montserrat, Gothic_A1 } from 'next/font/google'
 import './globals.css'
 import { PropsWithChildren } from 'react'
+import { Metadata } from 'next'
 
-const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
+const defaultUrl = process.env.VERCEL_URL
+	? `https://${process.env.VERCEL_URL}`
+	: 'http://localhost:3000'
 
-export const metadata = {
+export const metadata: Metadata = {
 	metadataBase: new URL(defaultUrl),
-	title: 'Hotely',
-	description: 'The fastest way to build apps with Next.js and Supabase'
+	title: {
+		template: 'Hotely - %s',
+		default: 'Hotely'
+	},
+	description:
+		'Discover and explore the best hotels around the world with Hotely. View detailed information, compare amenities, and find the perfect stay for your next trip.'
 }
 
-
-
 const montserrat = Montserrat({ subsets: ['latin'] })
-const gothic_A1 = Gothic_A1({ weight: ['700', '600', '500', '400'], subsets: ['latin'] })
+const gothic_A1 = Gothic_A1({
+	weight: ['700', '600', '500', '400'],
+	subsets: ['latin']
+})
 
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
-		<html lang='en' className={`${montserrat.className} ${gothic_A1.className}`}>
+		<html
+			lang='en'
+			className={`${montserrat.className} ${gothic_A1.className}`}
+		>
 			<body>
 				<main>{children}</main>
 			</body>
