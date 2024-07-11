a64f8f548048a194a39df8ba092db0d99d7283e0
selly361 <seikou615@gmail.com>
2024-07-11 10:52:42 +0100

    Refactor imports, rename `Logo` to `LightLogo`

--- components/Shared/Navbar/index.tsx
+++ components/Shared/Navbar/index.tsx
@@ -1,24 +1,24 @@
-import { default as Logo } from "@/public/svgs/logo.svg";
-import Button from "../Button";
+import LightLogo from '@/public/svgs/logo-light.svg'
+import { Button } from '@/components'
 
 const Navbar = () => {
-  return (
-    <header className="py-7 px-20 flex flex-row justify-center shadow-lg">
-      <nav className="flex max-w-screen-xl justify-between w-full">
-        <Logo />
-        <ul className="flex gap-8">
-          <li>
-            <Button variant="tertiary" className="font-semibold font-primary">
-              Login
-            </Button>
-          </li>
-          <li>
-            <Button className="font-semibold">Sign up</Button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+	return (
+		<header className='py-7 px-20 flex flex-row justify-center shadow-lg'>
+			<nav className='flex max-w-screen-xl justify-between w-full'>
+				<LightLogo />
+				<ul className='flex gap-8'>
+					<li>
+						<Button variant='tertiary' className='font-semibold'>
+							Login
+						</Button>
+					</li>
+					<li>
+						<Button className='font-semibold'>Sign up</Button>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	)
+}
 
-export default Navbar;
+export default Navbar
