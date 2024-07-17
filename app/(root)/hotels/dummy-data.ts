5911195b09b3dbf4fda316877278809258f78b2a
selly361 <seikou615@gmail.com>
2024-07-17 23:29:45 +0100

    Add `dummy-data` for hotels

---
+++ app/(root)/hotels/dummy-data.ts
@@ -0,0 +1,201 @@
+// hotelData.ts
+export const hotels = [
+	{
+		id: '1',
+		name: 'CVK Park Bosphorus Hotel Istanbul',
+		location: 'Gümüşsuyu Mah. İnönü Cad. No:8, Istanbul 34437',
+		rating: 4.2,
+		reviews: 371,
+		price: 240,
+		amenities: [
+			'5 Star Hotel',
+			'Outdoor pool',
+			'Indoor pool',
+			'Fitness center',
+			'Spa and wellness center',
+			'Restaurant',
+			'Bar/Lounge',
+			'Free Wi-Fi',
+			'Room service',
+			'Tea/coffee machine',
+			'24hr front desk',
+			'Free parking',
+			'Air-conditioned',
+			'Free internet',
+			'Free airport shuttle',
+			'Free cancellation',
+			'Multi-lingual staff',
+			'Concierge service'
+		],
+		thumbnail: '/images/hotels/thumbnail-1.png',
+		images: [
+			'/images/hotels/hotel-1/photo-1.png',
+			'/images/hotels/hotel-1/photo-2.png',
+			'/images/hotels/hotel-1/photo-3.png',
+			'/images/hotels/hotel-1/photo-4.png',
+			'/images/hotels/hotel-1/photo-5.png',
+			'/images/hotels/hotel-1/photo-6.png',
+			'/images/hotels/hotel-1/photo-7.png',
+			'/images/hotels/hotel-1/photo-8.png',
+			'/images/hotels/hotel-1/photo-9.png'
+		],
+		description:
+			'Located in Taksim Gümüşsuyu, the heart of Istanbul, the CVK Park Bosphorus Hotel Istanbul has risen from the ashes of the historic Park Hotel...',
+		rooms: [
+			{ type: 'Superior room - 1 double bed or 2 twin beds', price: 240 },
+			{
+				type: 'Superior room - City view - 1 double bed or 2 twin beds',
+				price: 280
+			},
+			{
+				type: 'Superior room - City view - 1 double bed or 2 twin beds',
+				price: 320
+			},
+			{
+				type: 'Superior room - City view - 1 double bed or 2 twin beds',
+				price: 350
+			}
+		]
+	},
+	{
+		id: '2',
+		name: 'Eresin Hotels Sultanahmet - Boutique Class',
+		location: 'Kucukayasofya No:40 Sultanahmet, Istanbul 34022',
+		rating: 4.2,
+		reviews: 54,
+		price: 104,
+		amenities: [
+			'5 Star Hotel',
+			'Outdoor pool',
+			'Fitness center',
+			'Restaurant',
+			'Bar/Lounge',
+			'Free Wi-Fi',
+			'Room service',
+			'Tea/coffee machine'
+		],
+		thumbnail: '/images/hotels/thumbnail-2.png',
+		images: [
+			'/public/images/hotels/hotel-2-1.png',
+			'/public/images/hotels/hotel-2-2.png',
+			'/public/images/hotels/hotel-2-3.png',
+			'/public/images/hotels/hotel-2-4.png',
+			'/public/images/hotels/hotel-2-5.png',
+			'/public/images/hotels/hotel-2-6.png',
+			'/public/images/hotels/hotel-2-7.png',
+			'/public/images/hotels/hotel-2-8.png',
+			'/public/images/hotels/hotel-2-9.png'
+		],
+		description:
+			'Situated in the heart of Sultanahmet, Eresin Hotels offers a unique blend of traditional charm and modern luxury...',
+		rooms: [
+			{ type: 'Superior room - 1 double bed or 2 twin beds', price: 104 },
+			{
+				type: 'Superior room - City view - 1 double bed or 2 twin beds',
+				price: 130
+			},
+			{
+				type: 'Deluxe room - City view - 1 double bed or 2 twin beds',
+				price: 150
+			},
+			{
+				type: 'Deluxe room - City view - 1 double bed or 2 twin beds',
+				price: 180
+			}
+		]
+	},
+	{
+		id: '3',
+		name: 'Eresin Hotels Sultanahmet - Boutique Class',
+		location: 'Kucukayasofya No:40 Sultanahmet, Istanbul 34022',
+		rating: 4.2,
+		reviews: 54,
+		price: 104,
+		amenities: [
+			'5 Star Hotel',
+			'Outdoor pool',
+			'Fitness center',
+			'Restaurant',
+			'Bar/Lounge',
+			'Free Wi-Fi',
+			'Room service',
+			'Tea/coffee machine'
+		],
+		thumbnail: '/images/hotels/thumbnail-3.png',
+		images: [
+			'/public/images/hotels/hotel-3-1.png',
+			'/public/images/hotels/hotel-3-2.png',
+			'/public/images/hotels/hotel-3-3.png',
+			'/public/images/hotels/hotel-3-4.png',
+			'/public/images/hotels/hotel-3-5.png',
+			'/public/images/hotels/hotel-3-6.png',
+			'/public/images/hotels/hotel-3-7.png',
+			'/public/images/hotels/hotel-3-8.png',
+			'/public/images/hotels/hotel-3-9.png'
+		],
+		description:
+			'Situated in the heart of Sultanahmet, Eresin Hotels offers a unique blend of traditional charm and modern luxury...',
+		rooms: [
+			{ type: 'Superior room - 1 double bed or 2 twin beds', price: 104 },
+			{
+				type: 'Superior room - City view - 1 double bed or 2 twin beds',
+				price: 130
+			},
+			{
+				type: 'Deluxe room - City view - 1 double bed or 2 twin beds',
+				price: 150
+			},
+			{
+				type: 'Deluxe room - City view - 1 double bed or 2 twin beds',
+				price: 180
+			}
+		]
+	},
+	{
+		id: '4',
+		name: 'Eresin Hotels Sultanahmet - Boutique Class',
+		location: 'Kucukayasofya No:40 Sultanahmet, Istanbul 34022',
+		rating: 4.2,
+		reviews: 54,
+		price: 104,
+		amenities: [
+			'5 Star Hotel',
+			'Outdoor pool',
+			'Fitness center',
+			'Restaurant',
+			'Bar/Lounge',
+			'Free Wi-Fi',
+			'Room service',
+			'Tea/coffee machine'
+		],
+		thumbnail: '/images/hotels/thumbnail-4.png',
+		images: [
+			'/public/images/hotels/hotel-4-1.png',
+			'/public/images/hotels/hotel-4-2.png',
+			'/public/images/hotels/hotel-4-3.png',
+			'/public/images/hotels/hotel-4-4.png',
+			'/public/images/hotels/hotel-4-5.png',
+			'/public/images/hotels/hotel-4-6.png',
+			'/public/images/hotels/hotel-4-7.png',
+			'/public/images/hotels/hotel-4-8.png',
+			'/public/images/hotels/hotel-4-9.png'
+		],
+		description:
+			'Situated in the heart of Sultanahmet, Eresin Hotels offers a unique blend of traditional charm and modern luxury...',
+		rooms: [
+			{ type: 'Superior room - 1 double bed or 2 twin beds', price: 104 },
+			{
+				type: 'Superior room - City view - 1 double bed or 2 twin beds',
+				price: 130
+			},
+			{
+				type: 'Deluxe room - City view - 1 double bed or 2 twin beds',
+				price: 150
+			},
+			{
+				type: 'Deluxe room - City view - 1 double bed or 2 twin beds',
+				price: 180
+			}
+		]
+	}
+]
